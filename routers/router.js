d3d19981dbe35717f31da7272b47ec61e79fd437
ErrolKeith <errol.k.keith@gmail.com>
2021-12-02 13:37:16 -0500

    render ejs instead of message.

--- routers/router.js
+++ routers/router.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 //navigation links routes
-router.get('/', function(req, res){res.send('home page')})
+router.get('/', function(req, res){res.render('index')})
 
 module.exports = router
