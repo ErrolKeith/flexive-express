ff30f8dcd2259cfe4592980851c9818826e0852f
ErrolKeith <errol.k.keith@gmail.com>
2021-12-03 14:03:18 -0500

    added admin route

--- routers/router.js
+++ routers/router.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+//Public routes
 router.get('/', function(req, res){ res.render('index') })
 router.get('/sitemap.xml', function (req, res) { res.sendFile("sitemap.xml", {root: "./"}) })
 router.get("/robots.txt", function (req, res) { res.sendFile("robots.txt", { root: "./" }) })
+
+//Private routes
+
+//Routes in robots.txt
+router.get('/admin', function (req, res) { res.send('admin login') })
 
 module.exports = router
